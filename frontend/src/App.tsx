50bc9e05fdc0eba4ca52d37f707678192dfff49f
ymakwan1 <ymakwan1@binghamton.edu>
2024-03-11 00:03:50 -0400

    tailwind added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// frontend/src/App.tsx
 
-function App() {
+import React from 'react';
+import UrlShortener from './components/UrlShortner';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 min-h-screen flex justify-center items-center">
+      <UrlShortener />
     </div>
   );
-}
+};
 
 export default App;
